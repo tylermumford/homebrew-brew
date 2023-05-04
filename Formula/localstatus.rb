011f547f09ea3a381ec67634794c664e9315a9e1
Tyler Mumford <tyler.mumford@icloud.com>
2023-05-04 17:01:10 -0600

    localstatus 0.2-beta

--- Formula/localstatus.rb
+++ Formula/localstatus.rb
@@ -1,8 +1,8 @@
 class Localstatus < Formula
   desc "Checks your local dev environment"
   homepage "https://github.com/tylermumford/localstatus"
-  url "https://github.com/tylermumford/localstatus/archive/refs/tags/0.1-alpha.tar.gz"
-  sha256 "983a41a0ff2b8b482485ebf46ac40f246ab7643a802bdf3da769e08baaba86eb"
+  url "https://github.com/tylermumford/localstatus/archive/refs/tags/v0.2-beta.tar.gz"
+  sha256 "aacf2b445ca5af8a24cbf67b77de9596a23eeca85e6a7c03cc0309a60c7bb613"
   license "BSD-3-Clause"
 
   depends_on "go"
